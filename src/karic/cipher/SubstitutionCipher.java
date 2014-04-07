9da71b4bf8f709b8cd83e2dfa168a10b32142316
Adin Karic <akaric@student.tgm.ac.at>
2014-04-07 22:40:22 +0200

    substitutionCipher-Klasse hinzugefügt

---
+++ src/karic/cipher/SubstitutionCipher.java
@@ -0,0 +1,13 @@
+package karic.cipher;
+
+public class SubstitutionCipher extends MonoAlphabeticCipher {
+	
+	public SubstitutionCipher(String secretAlphabet){
+		super.setSecretAlphabet(secretAlphabet);
+	}
+	@Override
+	public void setSecretAlphabet(String secretAlphabet){
+		super.setSecretAlphabet(secretAlphabet);
+	}
+	
+}
